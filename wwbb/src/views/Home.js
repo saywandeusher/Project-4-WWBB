a0e6e888b266d09b07d054df2f7d725d798d9fdf
saywandeusher <ongsaywan@hotmail.com>
2018-08-12 01:16:32 +0800

    fill home.js

--- wwbb/src/views/Home.js
+++ wwbb/src/views/Home.js
@@ -1,11 +1,27 @@
 import React from 'react'
+import LoginForm from '../components/loginform';
 
 export default class Home extends React.Component {
 
   render() {
     return (
-      <div className="Home">
-        <h1>Home</h1>
+      <div id="Home" className="main-content">
+
+        <div className="main-content-1">
+          <h1>Singapore's Best Entertainment Dance Crew</h1>
+        </div>
+
+        <div className="main-content-2">
+          <h2>We are a dance crew that consist of people from all over the neighbourhood of singapore!</h2>
+          <p>Over 100+ performance and counting!</p>
+        </div>
+
+        <div className="main-content-2">
+          <h2>Want a performance from us?</h2>
+          <p>Contact us now!</p>
+          <LoginForm />
+        </div>
+
       </div>
     );
   }
