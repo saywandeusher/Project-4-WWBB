3bf239014669630fd8d93c86576f3ab1d6dff978
saywandeusher <ongsaywan@hotmail.com>
2018-08-12 00:49:09 +0800

    shifted navbar into app.js

--- wwbb/src/App.js
+++ wwbb/src/App.js
@@ -1,7 +1,6 @@
 import React from  'react';
 import ReactDOM from 'react-dom';
-import LoginForm from './components/loginform'
-import NavBar from './components/navBar';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 import Media from './Media';
@@ -13,20 +12,46 @@ class App extends React.Component{
     this.state = {};
   }
 
-
-
   render() {
     return (
-      <div className="container">
-        <NavBar />
-        {/* hello!! this works! {this.props.greeting} */}
-      </div>
-    )
+      <Router>
+        <div className="container">
+            <div className="nav-logo">
+              <p>WWBB</p>
+            </div>
+
+            <div className="nav-bar">
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/about">About</Link>
+                </li>
+                <li>
+                  <Link to="/media">Media</Link>
+                </li>
+                <li>
+                  <Link to="/contact">Contact</Link>
+                </li>
+              </ul>
+            </div>
+
+            <hr />
+
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/media" component={Media} />
+            <Route path="/contact" component={Contact} />
+
+        </div> 
+      </Router>
+    );
   }
 }
 
 ReactDOM.render(
-  <App greeting="AT LONG LAST" />,
+  <App />,
   document.getElementById('app')
 )
 
